67e423bbf14bc6b7ace03aca56cbdfd7f69c48ce
Manabu Nakazawa <mshibanami+git@gmail.com>
2019-03-22 15:15:21 +1100

    add help for commandline

--- Sources/GitHubTrendingRSS/main.swift
+++ Sources/GitHubTrendingRSS/main.swift
@@ -1,7 +1,23 @@
 // Copyright (c) 2018 Manabu Nakazawa. Licensed under the MIT license. See LICENSE in the project root for license information.
 
 import Foundation
+import Darwin
 import GitHubTrendingRSSKit
+
+func checkCommandLineArguments() {
+    guard CommandLine.arguments.count == 3,
+        !CommandLine.arguments[1].isEmpty,
+        !CommandLine.arguments[2].isEmpty else {
+            fputs(
+                """
+        Error: The arguments are incorrect.
+        Please specify like this:
+            swift run GitHubTrendingRSS <GitHub Client ID> <GitHub Client Secret>\n
+        """, stderr)
+            exit(1)
+    }
+}
+checkCommandLineArguments()
 
 let downloadManager = DownloadManager()
 let gitHubDownloader = GitHubDownloader(
