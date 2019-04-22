50f5af047b608d4b0b677496a44ba04a073e7e08
Manabu Nakazawa <mshibanami+git@gmail.com>
2019-04-22 20:11:38 +1000

    run release build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 rm -rf output
-swift run GitHubTrendingRSS --client-id="${GITHUB_CLIENT_ID}" --client-secret="${GITHUB_CLIENT_SECRET}"
+swift run GitHubTrendingRSS --configuration=release --client-id="${GITHUB_CLIENT_ID}" --client-secret="${GITHUB_CLIENT_SECRET}"
 cp -r static output
