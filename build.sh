9e4b6a317c5d20685dc9c6e3198770da274034cb
Manabu Nakazawa <mshibanami+git@gmail.com>
2020-02-02 17:16:27 +1100

    GitHub doesn't allow me to use GITHUB_-prefiexed environment variables

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 rm -rf output
-swift run GitHubTrendingRSS --configuration=release --client-id="${GITHUB_CLIENT_ID}" --client-secret="${GITHUB_CLIENT_SECRET}"
+swift run GitHubTrendingRSS --configuration=release --client-id="${USER_GITHUB_CLIENT_ID}" --client-secret="${USER_GITHUB_CLIENT_SECRET}"
 cp -r static output
