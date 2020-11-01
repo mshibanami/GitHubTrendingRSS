5ca245919c0c8faa9c33ff0b98d0ac0dccac782b
Manabu Nakazawa <mshibanami+git@gmail.com>
2020-11-01 13:35:56 +1100

    nit

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ if [ -z "$USER_GITHUB_CLIENT_SECRET" ]; then
     echo "Please set the environment value 'USER_GITHUB_CLIENT_SECRET'." 1>&2
     exit 1
 fi
+
 rm -rf output
 swift run GitHubTrendingRSS --configuration=release --client-id="${USER_GITHUB_CLIENT_ID}" --client-secret="${USER_GITHUB_CLIENT_SECRET}"
 cp -r static output
